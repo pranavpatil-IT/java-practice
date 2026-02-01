a9fadb5aa77be78b773cabd85747a3710a8af744
Pranav patil <patilspranav1011@gmail.com>
2026-02-01 18:03:22 +0530

    Add files via upload

---
+++ literal.java
@@ -0,0 +1,16 @@
+public class literal {
+    public static void main(String[] args) {
+        byte age=10;
+        int age1=20;
+        float f1=2.4f;
+        double d1=3.44d;
+        short age3=34;
+        long age4=344432222L;
+        char ch='A';
+        boolean a=true;
+        System.out.println(age);
+        String str="pranav";
+        System.out.println(str);
+    }
+    
+}
