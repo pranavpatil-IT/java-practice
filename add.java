fff48aca07bcca9cde7f059cf7d783152d594ef7
pranavpatil-IT <patilspranav1011@gmail.com>
2026-02-01 14:42:07 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+
+//write a java program to add tree numbers.
+
+public class add {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+        int num3 = 8;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+    
+}
