24b9a98f039807ddf3f154cc1f3cc56fd9419bf4
Pranav patil <patilspranav1011@gmail.com>
2026-02-02 19:04:30 +0530

    Add files via upload

---
+++ Day02_TakingInpu.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+
+
+//import java.util.Scanner;
+
+//public class Day02_TakingInpu {
+//    public static void main(String[] args) {
+//    System.out.println("taking input from the user");
+//    Scanner sc=new Scanner(System.in);
+//    System.out.println("enter number 1");
+//        int a = sc.nextInt();
+//        System.out.println("enter number 2");
+//        int b = sc.nextInt();
+//        int sum=a+b;
+//       System.out.println("the sum of these number is= ");
+//        System.out.println(sum);
+//
+//  } 
+// }
+
+//FLOAT
+
+/*public class Day02_TakingInpu{
+    public static void main(String[]args){
+        System.out.println("taking input from the user");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number 1");
+        float a = sc.nextFloat();
+        System.out.println("enter number 2");
+        float b = sc.nextFloat();
+        float sum = a+b;
+        System.out.println(sum);
+    }
+
+}*/
+
+//BOOLEAN
+
+/*public class Day02_TakingInpu{
+     public static void main(String[] args) {
+         System.out.println("taking input from the user");
+         Scanner sc=new Scanner(System.in);
+         boolean b1=sc.hasNextInt();
+         System.out.println(b1);
+     }
+}*/
+
+
+
+
+/*public class Day02_TakingInpu{
+     public static void main(String[] args) {
+         System.out.println("taking input from the user");
+         Scanner sc=new Scanner(System.in);
+         String str =sc.next();
+         System.out.println(str);
+     }
+}*/
+
+
+
+public class Day02_TakingInpu{
+     public static void main(String[] args) {
+         System.out.println("taking input from the user");
+         Scanner sc=new Scanner(System.in);
+         String str =sc.nextLine();
+         System.out.println(str);
+     }
+}
+
