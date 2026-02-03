8f3dae54b495ab81ccb9d153866e2401e3ee7e5d
Pranav patil <patilspranav1011@gmail.com>
2026-02-03 19:25:19 +0530

    Add files via upload

---
+++ Day03operators.java
@@ -0,0 +1,30 @@
+public class Day03operators {
+    public static void main(String[] args) {
+        
+        //1.arithmetic operators 
+        int a=10; 
+        //int b=4+a;
+        //int b=14-a;
+        //int b=14 % a; //modulo operator
+        //int b=4*a;
+
+        //2.assignment operator
+        int b=4;
+        b +=4;
+        System.out.println(b);
+
+        //3.comparison operator
+        System.out.println(6==6);
+        System.out.println(64<6);
+
+        //4.logical operator
+        System.out.println(64>5 && 54>6);
+        System.out.println(63>4 && 4>54);
+        System.out.println(63>4 || 4>54);
+
+        //5.bitwise operator
+        System.out.println(2&3);
+
+    }
+    
+}
