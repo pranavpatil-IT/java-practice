9a4d1efde004b23f92818160fd476d81abc1a6cc
Pranav patil <patilspranav1011@gmail.com>
2026-02-05 16:23:39 +0530

    Add files via upload

---
+++ Day04datatype.java
@@ -0,0 +1,25 @@
+
+public class Day04datatype {
+    public static void main(String[] args) {
+     /* byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y+z;
+        float b = 3.4f + x;
+        System.out.println(a);
+        System.out.println(b);  */
+
+        // increment and decrement operators
+        int i =56;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+
+        //quick quiz
+        int y = 7;
+        int z = (++y)*(8);
+        System.out.println(z);
+    }
+    
+}
