bc2d1e4f62b6d27f763cfbe31bc65b1d17ef0972
Pranav patil <patilspranav1011@gmail.com>
2026-02-07 21:16:28 +0530

    Add files via upload

---
+++ Day06_conditionals.java
@@ -0,0 +1,12 @@
+public class Day06_conditionals {
+    public static void main(String[] args) {
+        int age = 20;
+        if(age != 18){      //(>,<,>=,<=,!=)  
+            System.out.println("yes you can drive");
+        }
+        else{
+            System.out.println("no you cant drive");
+        }
+    }
+    
+}
