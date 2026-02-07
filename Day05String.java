b98337417b2914369177526c5daff95d948039e1
Pranav patil <patilspranav1011@gmail.com>
2026-02-07 18:38:03 +0530

    Add files via upload

---
+++ Day05String.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Day05String {
+    public static void main(String[] args) {
+      //  String name = new String("pranav");
+      // String name="pranav";
+      //System.out.println("the name is ");
+    // int a=6;
+      //  float b=5.3f;
+        //System.out.printf("the value os a is %d and value of b is %f",a,b);
+
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
